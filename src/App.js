b5800fa4146db9f374269a2a9a1ad2c61a0ac2d4
graham187k <graham187k@gmail.com>
2018-09-29 20:21:26 -0400

    test

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
       return null;
     }
 
-    return (
-      <div className="App">
+    return ( 
+      <div className="App"><p>dsfs</p>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
